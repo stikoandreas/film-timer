3991aec5d5fbeab498efc2fabd34bd0c403770cf
Andreas Persson <stikoandreas@gmail.com>
2024-08-03 21:19:01 +0200

    Fixed 404 by configuring router basename

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+  ],
+  { basename: '/film-timer/' }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
