5a77c3d7abc2c24105b36bf442bfe7755bfd3766
Andreas Persson <stikoandreas@gmail.com>
2024-08-09 22:27:55 +0200

    Added desktop navbar

--- src/Root.tsx
+++ src/Root.tsx
@@ -6,19 +6,33 @@ import { DebugContextProvider } from '@/context/DebugContext';
 import { ButtonBar } from '@/components/ButtonBar/ButtonBar';
 
 import classes from './Root.module.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 export function Root() {
   return (
     <>
       <DebugContextProvider>
-        <AppShell header={{ height: 50 }} className={classes.shell}>
+        <AppShell
+          header={{ height: 50 }}
+          navbar={{
+            width: 50,
+            breakpoint: 'sm',
+          }}
+          footer={{
+            height: 50,
+          }}
+          className={classes.shell}
+        >
           <AppShell.Header className={classes.header}>
             <ButtonBar />
           </AppShell.Header>
+          <AppShell.Navbar visibleFrom="sm">
+            <Navbar />
+          </AppShell.Navbar>
           <AppShell.Main>
             <Outlet />
           </AppShell.Main>
-          <AppShell.Footer className={classes.footer}>
+          <AppShell.Footer className={classes.footer} hiddenFrom="sm">
             <MobileFooter />
           </AppShell.Footer>
         </AppShell>
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,44 @@
+import { IconAlarm, IconAlarmFilled, IconFlask, IconFlaskFilled } from '@tabler/icons-react';
+import { Link, useLocation } from 'react-router-dom';
+import { NavLink, Tooltip } from '@mantine/core';
+
+import classes from './Navbar.module.css';
+
+const links = [
+  {
+    label: 'Timer',
+    icon: <IconAlarm className={classes.icon} />,
+    selectedIcon: <IconAlarmFilled className={classes.icon} />,
+    href: '/',
+    id: 'footer-index',
+  },
+  {
+    label: 'Volume Calculator',
+    icon: <IconFlask />,
+    selectedIcon: <IconFlaskFilled />,
+    href: '/volume',
+    id: 'footer-volume',
+  },
+];
+
+export function Navbar() {
+  function isActive(link: string) {
+    return pathname === link;
+  }
+  const { pathname } = useLocation();
+  return (
+    <>
+      {links.map((link) => (
+        <Tooltip key={link.id} label={link.label} position="right" color="gray">
+          <NavLink
+            leftSection={isActive(link.href) ? link.selectedIcon : link.icon}
+            component={Link}
+            key={link.id}
+            to={link.href}
+            active={isActive(link.href)}
+          />
+        </Tooltip>
+      ))}
+    </>
+  );
+}
