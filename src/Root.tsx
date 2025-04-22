13d368fea4b0e8b160b6236be3585e86dbf91e57
Andreas Persson <stikoandreas@gmail.com>
2025-04-22 22:09:19 +0200

    Automatic wakelock

--- src/Root.tsx
+++ src/Root.tsx
@@ -7,41 +7,44 @@ import { ButtonBar } from '@/components/ButtonBar/ButtonBar';
 
 import classes from './Root.module.css';
 import { Navbar } from './components/Navbar/Navbar';
+import { WakeContextProvider } from './context/WakeContext';
 
 export function Root() {
   const { pathname } = useLocation();
   return (
     <>
       <DebugContextProvider>
-        <AppShell
-          header={{ height: 50 }}
-          navbar={{
-            width: 50,
-            breakpoint: 'sm',
-            collapsed: { mobile: true },
-          }}
-          footer={{
-            height: 65,
-          }}
-          className={classes.shell}
-        >
-          <AppShell.Header className={classes.header}>
-            <ButtonBar />
-          </AppShell.Header>
-          <AppShell.Navbar visibleFrom="sm" className={classes.navbar}>
-            <Navbar />
-          </AppShell.Navbar>
-          <AppShell.Main className={classes.main}>
-            <div className={pathname !== '/timer' ? classes.container : undefined}>
-              <Outlet />
-            </div>
-          </AppShell.Main>
-          {pathname !== '/timer' && (
-            <AppShell.Footer className={classes.footer} hiddenFrom="sm">
-              <MobileFooter />
-            </AppShell.Footer>
-          )}
-        </AppShell>
+        <WakeContextProvider>
+          <AppShell
+            header={{ height: 50 }}
+            navbar={{
+              width: 50,
+              breakpoint: 'sm',
+              collapsed: { mobile: true },
+            }}
+            footer={{
+              height: 65,
+            }}
+            className={classes.shell}
+          >
+            <AppShell.Header className={classes.header}>
+              <ButtonBar />
+            </AppShell.Header>
+            <AppShell.Navbar visibleFrom="sm" className={classes.navbar}>
+              <Navbar />
+            </AppShell.Navbar>
+            <AppShell.Main className={classes.main}>
+              <div className={pathname !== '/timer' ? classes.container : undefined}>
+                <Outlet />
+              </div>
+            </AppShell.Main>
+            {pathname !== '/timer' && (
+              <AppShell.Footer className={classes.footer} hiddenFrom="sm">
+                <MobileFooter />
+              </AppShell.Footer>
+            )}
+          </AppShell>
+        </WakeContextProvider>
       </DebugContextProvider>
     </>
   );
